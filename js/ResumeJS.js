01805b02ffde07b2e18d9a17a8f04f95c678b244
CalvinThai44 <CalvinThai44@gmail.com>
2023-07-30 20:59:25 +1000

    Added side bar, new sections and filled text
    - Fixed and added js css and html code for side bar
    - Added about me, skills and experience tab.
    - Formatting and styling of landing page.
    - Content for about me page.

--- js/ResumeJS.js
+++ js/ResumeJS.js
@@ -1,13 +1,12 @@
 
-//Animated scroll to div
-
+//Animated scroll to id
 const scrollSmoothToElement = (id) => {
-	
-    const element = document.getElementById(id);
-	element.scrollTop = element.scrollHeight;
+		const yPos = id.getBoundingClientRect().top + window.scrollY;
+		console.log(document.documentElement.clientHeight)	
+		document.documentElement.scrollTop += yPos;;
 }
 
-const scrollSmoothToTop = (id) => {
-	const element = document.getElementById(id);
-	element.scrollTop = 0;
+//Animated scroll to top
+const scrollSmoothToTop = () => {
+	document.body.scrollTop = document.documentElement.scrollTop = 0;
 }
